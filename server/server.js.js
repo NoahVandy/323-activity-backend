5c671b8ca00a3e17784c213f9ab895c065f18b16
Noah's work laptop <nvandervel@my.gcu.edu>
2021-01-21 11:10:22 -0700

    renamed index to server

--- server/server.js.js
+++ server/server.js.js
@@ -13,15 +13,15 @@ const http = require('http');
 //   database: '323-activity',
 // })
 
-// const connection = mysql.createConnection({
-//   host: "127.0.0.1",
-//   port: 52951,
-//   user: "azure",
-//   password: "6#vWHD_$",
-//   database: "localdb",
-// })
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 52951,
+  user: "azure",
+  password: "6#vWHD_$",
+  database: "localdb",
+})
 
-// connection.connect();
+connection.connect();
 
 app.use(bodyParser.urlencoded({
   extended: true,
